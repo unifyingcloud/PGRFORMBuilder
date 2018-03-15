a04641a42b67104962deb38c93fb06045e1550de
unifyingcloud <jonatane.cisneros@gmail.com>
2018-03-15 17:55:31 +0100

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -44,10 +44,7 @@ jQuery(function($) {
  
     var formData = `[
  
-
-{"type":"text","subtype":"text","description":"FED/SEIDF/ UEIDT-  /000000/2017","label":"​NÚMERO DE AVERIGUACIÓN PREVIA O CARPETA DE INVESTIGACION","className":"formControl"}
-,
-{"type":"date","name":"date2","label":"II)FECHA DE INICIO DE LA CARPETA DE INVESTIGACION", "class":"formControl"}
+{"type":"text","subtype":"text","description":"FED/SEIDF/ UEIDT-  /000000/2017","label":"​NÚMERO DE AVERIGUACIÓN PREVIA O CARPETA DE INVESTIGACION","className":"formControl"},{"type":"date","name":"date2","label":"II)FECHA DE INICIO DE LA CARPETA DE INVESTIGACION", "class":"formControl"},{"type":"radio-group","name":"radiogroup3","label":"III)DELITO POR EL QUE SE INICIA LA CARPETA DE INVESTIGACIÓN","values":[{"label":"INHUMANOS O DEGRADANTES","value":"INHUMANOS O DEGRADANTES"},{"label":"TRATOS O PENAS CRUELES","value":"TRATOS O PENAS CRUELES"},{"label":"DELITOS VINCULADOS DESCRITOS EN LOS ART. 30 Y 31 DE LA LEY GENERAL","value":"DELITOS VINCULADOS DESCRITOS EN LOS ART. 30 Y 31 DE LA LEY GENERAL"},{"label":"TORTURA","value":"TORTURA"}]},{"type":"date","name":"date4","label":"IV) DATOS DE LA INVESTIGACION DE ORIGEN"},{"type":"radio-group","name":"radiogroup5","label":"JUZGADO","values":[]},{"type":"radio-group","name":"radiogroup6","label":"PROCESO","values":[]}
 
 ]`
   
